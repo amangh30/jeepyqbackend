4e79422782d6d885f32bbc4069da2f4253b9c547
Aman <108615374+amangh30@users.noreply.github.com>
2024-03-22 13:19:53 +0530

    Update getpyq.js

--- controller/getpyq.js
+++ controller/getpyq.js
@@ -18,15 +18,30 @@ export const getallpyq = async(req,res)=>{
 
 
 export const getfile=async(req,res)=>{
-    const fileName = req.params.filename;
-  const filePath = `/tmp/${fileName}`;
-    console.log(filePath)
-  
-    fs.exists(filePath, (exists) => {
-      if (exists) {
-        res.download(filePath);
-      } else {
-        res.status(404).send('File not found.');
-      }
+  const fileId = req.params.id;
+  try {
+    // Check if fileId is a valid ObjectId
+    if (!mongoose.Types.ObjectId.isValid(fileId)) {
+      return res.status(400).send('Invalid file ID.');
+    }
+
+    // Find file in MongoDB using Mongoose
+    const file = await Pyq.findById(fileId).exec();
+    if (!file) {
+      return res.status(404).send('File not found.');
+    }
+
+    // Set response headers
+    res.set({
+      'Content-Type': file.file.contentType,
+      'Content-Disposition': `attachment; filename="${file.file.filename}"`
     });
-};
+
+    // Send file data
+    res.send(file.file.data);
+  } catch (err) {
+    console.error('Error downloading file:', err);
+    res.status(500).send('Error downloading file.');
+  }
+}
+
