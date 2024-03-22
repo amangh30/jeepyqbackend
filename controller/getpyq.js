475e0e6f96ce8ffa15f25580dbe4c82c54b7bf0c
Aman <108615374+amangh30@users.noreply.github.com>
2024-03-22 14:12:51 +0530

    Update getpyq.js

--- controller/getpyq.js
+++ controller/getpyq.js
@@ -10,8 +10,7 @@ export const getallpyq = async(req,res)=>{
  try {
         // Fetch data from the database
         const data = await Pyq.find();
-         console.log(data)
-         return res.status(200).send('Invalid file ID.');
+         return res.status(200).json(data);
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
